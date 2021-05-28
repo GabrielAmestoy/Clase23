ee3a785d2e64ef872ce4f3ac039f283447c16b96
GabrielAmestoy <gabrielamestoy1993@gmail.com>
2021-05-28 15:01:12 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const express = require("express"); // requiero express
+const app = express(); //requiero la funcinalidad de express
+app.listen(3000, () => console.log("Servidor funcionando en puerto 3000"))
